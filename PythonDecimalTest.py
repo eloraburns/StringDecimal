c4d94cd0feee59949e790d700e254211096429ed
Taavi Burns <taavi@taaviburns.ca>
2010-11-04 10:00:25 -0400

    Added tests for divide; no working JS yet.

--- PythonDecimalTest.py
+++ PythonDecimalTest.py
@@ -1,4 +1,4 @@
-from decimal import Decimal, getcontext
+from decimal import Decimal, getcontext, DivisionByZero, ROUND_HALF_UP
 import json
 
 with open('tests.json') as f:
@@ -7,20 +7,36 @@ with open('tests.json') as f:
     tests = json.loads(f.read())
 
 getcontext().prec = 100
+getcontext().rounding = ROUND_HALF_UP
 
-for operation, op1, op2, expected in tests:
+for test in tests:
+    operation = test[0]
+    ops = test[1:-1]
+    expected = test[-1]
     if operation == 'add':
-        actual = str(Decimal(op1) + Decimal(op2))
+        actual = str(Decimal(ops[0]) + Decimal(ops[1]))
     elif operation == 'subtract':
-        actual = str(Decimal(op1) - Decimal(op2))
+        actual = str(Decimal(ops[0]) - Decimal(ops[1]))
     elif operation == 'multiply':
-        actual = str(Decimal(op1) * Decimal(op2))
+        actual = str(Decimal(ops[0]) * Decimal(ops[1]))
+    elif operation == 'divide':
+        oldprec = getcontext().prec
+        newprec = int(ops[2])+len(ops[0])+len(ops[1])+5
+        getcontext().prec = newprec
+        try:
+            quantizer = Decimal("1E-%s" % ops[2])
+            dividend = Decimal(ops[0]) / Decimal(ops[1])
+            actual = str(dividend.quantize(quantizer))
+        except DivisionByZero:
+            actual = "NaN";
+        finally:
+            getcontext().prec = oldprec
     elif operation == 'round':
-        actual = str(Decimal(op1).quantize(Decimal("1E-%s" % op2)))
+        actual = str(Decimal(ops[0]).quantize(Decimal("1E-%s" % ops[1])))
     else:
         print "'%s' NOT IMPLEMENTED" % operation
 
     try:
-        assert expected == actual, "%s('%s', '%s') expected '%s', actual '%s'" % (operation, op1, op2, expected, actual)
+        assert expected == actual, "%s(%s) expected '%s', actual '%s'" % (operation, ', '.join("'%s'" % o for o in ops), expected, actual)
     except AssertionError as e:
         print e
