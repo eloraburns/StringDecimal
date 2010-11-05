39b78d0c0198732893491b79b2150fa576013932
Taavi Burns <taavi@taaviburns.ca>
2010-11-04 22:10:22 -0400

    Rewrite how StringDecimal is created and fix some jslint complaints.  (divide will be back shortly)

--- StringDecimal.js
+++ StringDecimal.js
@@ -1,38 +1,36 @@
 /* Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php */
-var StringDecimal = (function(){
-	var o = {};
-
-	o._copy = function(a) {
+var StringDecimal = {
+	_copy: function(a) {
 		return {
 			'sign': a.sign,
 			'mantissa': a.mantissa.slice(),
 			'exponent': a.exponent
-		}
-	}
+		};
+	},
 
-	o._string_to_array = function(str) {
+	_string_to_array: function(str) {
 		var result = [];
 		for (var i = 0; i < str.length; i++) {
 			result.push(parseInt(str[i], 10));
 		}
 		return result;
-	};
+	},
 
-	o._array_to_string = function(arr) {
+	_array_to_string: function(arr) {
 		var result = "";
 		for (var i = 0; i < arr.length; i++) {
 			result += arr[i];
 		}
 		return result;
-	};
+	},
 
-	o._array_fill = function(length, value) {
+	_array_fill: function(length, value) {
 		var result = [];
 		while (length--) {
 			result.push(value);
 		}
 		return result;
-	}
+	},
 
 	/*
 	 * Parses a string into a sign, mantissa, and exponent.
@@ -40,31 +38,31 @@ var StringDecimal = (function(){
 	 * of ten required to turn the raw mantissa into the real
 	 * value.  e.g. {mantissa: [1, 0], exponent: 1} => '1.0'
 	 */
-	o._parse = function(str) {
+	_parse: function(str) {
 		var matches = str.match(/^(\+|-)?0*(\d+)(?:\.(\d+))?$/);
 		var sign = matches[1] || '+';
-		var fractional = (undefined == matches[3]) ? "" : matches[3];
+		var fractional = (undefined === matches[3]) ? "" : matches[3];
 		var mantissa_as_a_string = matches[2] + fractional;
 		var exponent = fractional.length;
 		return {
 			'sign': sign,
-			'mantissa': o._string_to_array(mantissa_as_a_string),
+			'mantissa': StringDecimal._string_to_array(mantissa_as_a_string),
 			'exponent': exponent
 		};
-	};
+	},
 
-	o._format = function(obj) {
+	_format: function(obj) {
 		var sign = (obj.sign == "-") ? "-" : "";
-		var mantissa = o._array_to_string(obj.mantissa);
+		var mantissa = StringDecimal._array_to_string(obj.mantissa);
 		var decimal_point = (obj.exponent > 0) ? "." : "";
 		var decimal_point_offset = mantissa.length - obj.exponent;
 		return sign +
 			mantissa.substr(0, decimal_point_offset) +
 			decimal_point + 
 			mantissa.substr(decimal_point_offset);
-	};
+	},
 
-	o._array_add = function(a, b) {
+	_array_add: function(a, b) {
 		if (a.length != b.length) {
 			throw "Arrays of dissimilar length cannot be added";
 		}
@@ -73,17 +71,17 @@ var StringDecimal = (function(){
 			result.push(a[i] + b[i]);
 		}
 		return result;
-	};
+	},
 
-	o._array_multiply = function(arr, n) {
+	_array_multiply: function(arr, n) {
 		var result = [];
 		for (var i = 0; i < arr.length; i++) {
 			result.push(arr[i] * n);
 		}
 		return result;
-	}
+	},
 
-	o._match_exponents = function(a, b) {
+	_match_exponents: function(a, b) {
 		while (a.exponent > b.exponent) {
 			b.exponent++;
 			b.mantissa.push(0);
@@ -92,9 +90,9 @@ var StringDecimal = (function(){
 			a.exponent++;
 			a.mantissa.push(0);
 		}
-	};
+	},
 
-	o._match_leading = function(a, b) {
+	_match_leading: function(a, b) {
 		if (a.exponent != b.exponent) {
 			throw "Can't match leading with different exponents";
 		}
@@ -104,16 +102,16 @@ var StringDecimal = (function(){
 		while (b.mantissa.length > a.mantissa.length) {
 			a.mantissa.unshift(0);
 		}
-	};
+	},
 
-	o._strip_leading = function(a) {
-		while (a.mantissa.length-1 > a.exponent && a.mantissa[0] == 0) {
+	_strip_leading: function(a) {
+		while (a.mantissa.length-1 > a.exponent && a.mantissa[0] === 0) {
 			a.mantissa.shift();
 		}
 		return a;
-	}
+	},
 
-	o._carry = function(arr) {
+	_carry: function(arr) {
 		var carry = 0;
 		var result = [];
 		var current = 0;
@@ -128,56 +126,56 @@ var StringDecimal = (function(){
 		if (carry > 0) {
 			result.push(carry);
 		}
-		result.reverse()
+		result.reverse();
 		return result;
-	};
+	},
 
-	o.add = function(raw_a, raw_b) {
-		var a = o._parse(raw_a);
-		var b = o._parse(raw_b);
+	add: function(raw_a, raw_b) {
+		var a = StringDecimal._parse(raw_a);
+		var b = StringDecimal._parse(raw_b);
 		var sum;
-		o._match_exponents(a, b);
-		o._match_leading(a, b);
+		StringDecimal._match_exponents(a, b);
+		StringDecimal._match_leading(a, b);
 		if (a.sign == b.sign) {
 			sum = {
 				'sign': a.sign,
-				'mantissa': o._carry(o._array_add(a.mantissa, b.mantissa)),
+				'mantissa': StringDecimal._carry(StringDecimal._array_add(a.mantissa, b.mantissa)),
 				'exponent': a.exponent
-			}
+			};
 		} else {
 			// You, too, can run the tests to convince yourself that this is correct. :)
 			if (a.mantissa > b.mantissa) {
 				sum = {
 					'sign': a.sign,
-					'mantissa': o._carry(o._array_add(a.mantissa, o._array_multiply(b.mantissa, -1))),
+					'mantissa': StringDecimal._carry(StringDecimal._array_add(a.mantissa, StringDecimal._array_multiply(b.mantissa, -1))),
 					'exponent': a.exponent
-				}
+				};
 			} else if (a.mantissa < b.mantissa) {
 				sum = {
 					'sign': b.sign,
-					'mantissa': o._carry(o._array_add(o._array_multiply(a.mantissa, -1), b.mantissa)),
+					'mantissa': StringDecimal._carry(StringDecimal._array_add(StringDecimal._array_multiply(a.mantissa, -1), b.mantissa)),
 					'exponent': a.exponent
-				}
+				};
 			} else {
 				sum = {
 					'sign': '+',
-					'mantissa': o._array_fill(a.mantissa.length, 0),
+					'mantissa': StringDecimal._array_fill(a.mantissa.length, 0),
 					'exponent': a.exponent
-				}
+				};
 			}
 		}
-		return o._format(o._strip_leading(sum));
-	};
+		return StringDecimal._format(StringDecimal._strip_leading(sum));
+	},
 
-	o.subtract = function(raw_a, raw_b) {
-		var b = o._parse(raw_b);
+	subtract: function(raw_a, raw_b) {
+		var b = StringDecimal._parse(raw_b);
 		b.sign = (b.sign == "-") ? "+" : "-";
-		return o.add(raw_a, o._format(b));
-	}
+		return StringDecimal.add(raw_a, StringDecimal._format(b));
+	},
 
-	o.multiply = function(raw_a, raw_b) {
-		var a = o._parse(raw_a);
-		var b = o._parse(raw_b);
+	multiply: function(raw_a, raw_b) {
+		var a = StringDecimal._parse(raw_a);
+		var b = StringDecimal._parse(raw_b);
 		var product = {};
 		if (a.sign == b.sign) {
 			product.sign = '+';
@@ -195,24 +193,24 @@ var StringDecimal = (function(){
 		// Except that rfiller and lfiller put zeros in the spaces so we can just add everything together.
 		// _carry() happily takes very large values to carry, and we shouldn't ever need more than the one
 		// extra digit _carry() will give us at the end.
-		var rfiller = o._array_fill(b.mantissa.length-1, 0);
+		var rfiller = StringDecimal._array_fill(b.mantissa.length-1, 0);
 		var lfiller = [];
 		var addends = [];
 		for (var i = 0; i < b.mantissa.length; i++) {
-			addends.push(lfiller.concat(o._array_multiply(a.mantissa, b.mantissa[i]).concat(rfiller)));
+			addends.push(lfiller.concat(StringDecimal._array_multiply(a.mantissa, b.mantissa[i]).concat(rfiller)));
 			lfiller.push(rfiller.pop());
 		}
 		var acc = addends.pop();
 		while (addends.length) {
-			acc = o._array_add(acc, addends.pop());
+			acc = StringDecimal._array_add(acc, addends.pop());
 		}
-		product.mantissa = o._carry(acc);
+		product.mantissa = StringDecimal._carry(acc);
 		product.exponent = a.exponent + b.exponent;
-		return o._format(o._strip_leading(product));
-	}
+		return StringDecimal._format(StringDecimal._strip_leading(product));
+	},
 
-	o.round = function(raw_a, places) {
-		var a = o._parse(raw_a);
+	round: function(raw_a, places) {
+		var a = StringDecimal._parse(raw_a);
 		if (a.exponent > places) {
 			while (a.exponent > places+1) {
 				a.exponent--;
@@ -222,7 +220,7 @@ var StringDecimal = (function(){
 			var rounding_digit = a.mantissa.pop();
 			if (rounding_digit >= 5) {
 				a.mantissa[a.mantissa.length-1]++;
-				a.mantissa = o._carry(a.mantissa);
+				a.mantissa = StringDecimal._carry(a.mantissa);
 			}
 		} else {
 			while (a.exponent < places) {
@@ -230,17 +228,17 @@ var StringDecimal = (function(){
 				a.mantissa.push(0);
 			}
 		}
-		return o._format(a);
-	};
+		return StringDecimal._format(a);
+	},
 
-	o._all_zero = function(a) {
+	_all_zero: function(a) {
 		for (var i = 0; i < a.length; i++) {
-			if (a[i] != 0) {
+			if (a[i] !== 0) {
 				return false;
 			}
 		}
 		return true;
-	};
+	},
 
 	o.divide = function(raw_a, raw_b, places) {
 		return raw_a;
@@ -248,3 +246,4 @@ var StringDecimal = (function(){
 
 	return o;
 })();
+};
