534b4b3073ba5907d60ed163830e840f16d738a1
Taavi Burns <taavi@taaviburns.ca>
2010-11-05 22:10:32 -0400

    Implemented _parse, with it's dependencies, plus tests.

--- StringDecimal.php
+++ StringDecimal.php
@@ -1,4 +1,53 @@
 <?php
 
 class StringDecimal {
+
+	function _copy($a) {
+		return array(
+			'sign' => $a['sign'],
+			'mantissa' => array_slice($a['mantissa'], 0),
+			'exponent' => $a['exponent']
+		);
+	}
+
+	function _string_to_array($str) {
+		return array_map('intval', str_split($str));
+	}
+
+	function _strip_leading($a) {
+		while (count($a['mantissa'])-1 > $a['exponent'] && $a['mantissa'][0] === 0) {
+			array_shift($a['mantissa']);
+		}
+		return $a;
+	}
+
+	function _parse($str) {
+		preg_match("/^(\+|-)?0*(\d+)(?:\.(\d+))?(?:e((?:\+|-)?\d+))?$/i", $str, $matches);
+		$sign = $matches[1] ? $matches[1] : '+';
+		$fractional = strlen($matches[3]) == 0 ? "" : $matches[3];
+		$mantissa_as_a_string = $matches[2] . $fractional;
+		$adjust = strlen($matches[4]) == 0 ? 0 : intval($matches[4]);
+		$exponent = strlen($fractional);
+		$value = array(
+			'sign' => $sign,
+			'mantissa' => $this->_string_to_array($mantissa_as_a_string),
+			'exponent' => $exponent
+		);
+		while ($adjust > 0) {
+			if ($value['exponent'] > 0) {
+				$value['exponent']--;
+			} else {
+				array_push($value['mantissa'], 0);
+			}
+			$adjust--;
+		}
+		while ($adjust < 0) {
+			$value['exponent']++;
+			if ($value['exponent'] >= count($value['mantissa'])) {
+				array_unshift($value['mantissa'], 0);
+			}
+			$adjust++;
+		}
+		return $this->_strip_leading($value);
+	}
 }
--- tests/php/StringDecimalTest.php
+++ tests/php/StringDecimalTest.php
@@ -3,8 +3,114 @@
 require_once("../../StringDecimal.php");
 
 class StringDecimalTest extends PHPUnit_Framework_TestCase {
+
 	function setup() {
 		$this->sd = new StringDecimal();
+	}
+
+	function test__string_to_array() {
+		$this->assertEquals(array(0), $this->sd->_string_to_array("0"));
+		$this->assertEquals(array(1), $this->sd->_string_to_array("1"));
+		$this->assertEquals(array(9), $this->sd->_string_to_array("9"));
+		$this->assertEquals(array(1, 0), $this->sd->_string_to_array("10"));
+		$this->assertEquals(array(1, 9, 2, 8, 3, 7, 4, 6, 5, 0), $this->sd->_string_to_array("1928374650"));
+	}
+
+	function test__strip_leading() {
+		$base = array(
+			'sign' => '+',
+			'mantissa' => array(0),
+			'exponent' => 0
+		);
+
+		$a = $this->sd->_copy($base);
+		$b = $this->sd->_copy($base);
+		$this->assertEquals($b, $this->sd->_strip_leading($a), "zero to zero");
+
+		$a = $this->sd->_copy($base);
+		$a['mantissa'] = array(0, 0);
+		$b = $this->sd->_copy($base);
+		$this->assertEquals($b, $this->sd->_strip_leading($a), "zerozero to zero");
+
+		$a = $this->sd->_copy($base);
+		$a['mantissa'] = array(0, 0);
+		$a['exponent'] = 1;
+		$b = $this->sd->_copy($base);
+		$b['mantissa'] = array(0, 0);
+		$b['exponent'] = 1;
+		$this->assertEquals($b, $this->sd->_strip_leading($a), "zero.zero to zero.zero");
+
+		$a = $this->sd->_copy($base);
+		$a['mantissa'] = array(0, 1, 0);
+		$a['exponent'] = 1;
+		$b = $this->sd->_copy($base);
+		$b['mantissa'] = array(1, 0);
+		$b['exponent'] = 1;
+		$this->assertEquals($b, $this->sd->_strip_leading($a), "zerozero.zero to zero.zero");
+	}
+
+	function test__parse() {
+		$this->assertEquals($this->sd->_parse("0"), array(
+			'sign' => '+',
+			'mantissa' => array(0),
+			'exponent' => 0
+		), "zero");
+		$this->assertEquals($this->sd->_parse("-0"), array(
+			'sign' => '-',
+			'mantissa' => array(0),
+			'exponent' => 0
+		), "negative zero");
+		$this->assertEquals($this->sd->_parse("1"), array(
+			'sign' => '+',
+			'mantissa' => array(1),
+			'exponent' => 0
+		), "one");
+		$this->assertEquals($this->sd->_parse("+1"), array(
+			'sign' => '+',
+			'mantissa' => array(1),
+			'exponent' => 0
+		), "positive one");
+		$this->assertEquals($this->sd->_parse("-1"), array(
+			'sign' => '-',
+			'mantissa' => array(1),
+			'exponent' => 0
+		), "negative one");
+		$this->assertEquals($this->sd->_parse("10"), array(
+			'sign' => '+',
+			'mantissa' => array(1, 0),
+			'exponent' => 0
+		), "ten");
+		$this->assertEquals(array(
+			'sign' => '+',
+			'mantissa' => array(1, 0),
+			'exponent' => 1
+		), $this->sd->_parse("1.0"), "one point aught");
+		$this->assertEquals($this->sd->_parse("-1.0"), array(
+			'sign' => '-',
+			'mantissa' => array(1, 0),
+			'exponent' => 1
+		), "negative one point aught");
+		$this->assertEquals($this->sd->_parse("0e-1"), array(
+			'sign' => '+',
+			'mantissa' => array(0, 0),
+			'exponent' => 1
+		), "0e-1");
+		$this->assertEquals($this->sd->_parse("1e-1"), array(
+			'sign' => '+',
+			'mantissa' => array(0, 1),
+			'exponent' => 1
+		), "1e-1");
+		$this->assertEquals($this->sd->_parse("1e+1"), array(
+			'sign' => '+',
+			'mantissa' => array(1, 0),
+			'exponent' => 0
+		), "1e+1");
+		$this->assertEquals($this->sd->_parse("1e1"), array(
+			'sign' => '+',
+			'mantissa' => array(1, 0),
+			'exponent' => 0
+		), "1e1");
+
 	}
 
 	static function shared_testcases() {
@@ -30,4 +136,5 @@ class StringDecimalTest extends PHPUnit_Framework_TestCase {
 		$expected = array_pop($args);
 		$this->assertEquals($expected, call_user_func_array(array($this->sd, $method), $args));
 	}
+
 }
